6c415944eaba636ad6d94d52bda3b67fa72ddb01
Dmitriy Savin <mr.dima337@gmail.com>
2025-10-01 14:55:13 +0300

    feat: add JSX and clickEvent in ListGroup

--- React-app/src/components/ListGroup.tsx
+++ React-app/src/components/ListGroup.tsx
@@ -1,12 +1,34 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
+  let items = [
+    "Detroit",
+    "Majorka",
+    "San-Francisko",
+    "Moscow",
+    "Saint-Petersburg",
+  ];
+
+  const handleClick = (e: MouseEvent) => console.log(e);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List group</h1>
+
+      {!items.length && <p>No item found</p>}
+
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={crypto.randomUUID()}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
