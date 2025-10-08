e8e6ab3e593b35e7a412c71b11a386872fb422b4
Dmitriy Savin <mr.dima337@gmail.com>
2025-10-08 12:18:08 +0300

    feat: implement controlled form elements

--- React-app/src/components/Form.tsx
+++ React-app/src/components/Form.tsx
@@ -1,21 +1,41 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
-
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
-          some label
+          name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          value={person.name}
+          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label"></label>
-        <input id="age" type="number" className="form-control" />
+        <label htmlFor="age" className="form-label">
+          age
+        </label>
+        <input
+          value={person.age}
+          onChange={(e) =>
+            setPerson({ ...person, age: Number(e.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
