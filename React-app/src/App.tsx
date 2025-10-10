9424b22d25646f5ab200d3ca00dd17be0cf2d57a
Dmitriy Savin <mr.dima337@gmail.com>
2025-10-10 14:32:23 +0300

    feat: implement validation ExpenseForm

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import Form from "./components/Form";
-import "./index.css";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Entertainment", "Utilities"];
+import categories from "./expense-tracker/categories";
 
 function App() {
   const [expenses, setExpenses] = useState([
