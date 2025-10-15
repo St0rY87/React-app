2c172e5d252d1b61029afeab1bfbb284f75d19a3
Dmitriy <mr.dima337@gmail.com>
2025-10-15 15:12:30 +0300

    understanding the Effect hook

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
@@ -22,14 +22,27 @@ function App() {
     ? expenses.filter((expense) => expense.category === selectedCategory)
     : expenses;
 
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  });
+
+  useEffect(()=> {
+    document.title = 'My App'
+  })
+
   return (
     <div>
-      <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1 }])} />
+      {/* <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1 }])} />
       <ExpenseFilter
         onSelectCategory={(category) => setSelectedCategory(category)}
       />
       <ExpenseList expenses={visibleExpenses} onDelete={deleteItem} />
-      
+       */}
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
