4bbeb5107c3059562d2e93b7a01ef460f87371c8
Dmitriy <mr.dima337@gmail.com>
2025-10-16 10:13:01 +0300

    feat: install axios

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -5,26 +5,33 @@ import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import categories from "./expense-tracker/categories";
 import ProductList from "./components/ProductList";
 
+const connect = () => console.log("connecting");
+const disconnect = () => console.log("disconnecting");
+
 function App() {
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Groceries" },
-    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
-    { id: 4, description: "ddd", amount: 10, category: "Entertainment" },
-  ]);
+  // const [expenses, setExpenses] = useState([
+  //   { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+  //   { id: 2, description: "bbb", amount: 10, category: "Groceries" },
+  //   { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+  //   { id: 4, description: "ddd", amount: 10, category: "Entertainment" },
+  // ]);
 
-  const [selectedCategory, setSelectedCategory] = useState("");
+  // const [selectedCategory, setSelectedCategory] = useState("");
 
-  const deleteItem = (id: number) => {
-    setExpenses(expenses.filter((expense) => expense.id !== id));
-  };
+  // const deleteItem = (id: number) => {
+  //   setExpenses(expenses.filter((expense) => expense.id !== id));
+  // };
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((expense) => expense.category === selectedCategory)
-    : expenses;
+  // const visibleExpenses = selectedCategory
+  //   ? expenses.filter((expense) => expense.category === selectedCategory)
+  //   : expenses;
 
-  useEffect(() => {});
-  const [category, setCategory] = useState('')
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  });
+  // const [category, setCategory] = useState("");
   return (
     <div>
       {/* <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1 }])} />
@@ -33,12 +40,12 @@ function App() {
       />
       <ExpenseList expenses={visibleExpenses} onDelete={deleteItem} />
        */}
-         <select onChange={e => setCategory( e.target.value)} className="form-select">
+      {/* <select onChange={e => setCategory( e.target.value)} className="form-select">
                 <option value=""></option>
                 <option value="Clothing">Clothing</option>
                 <option value="Household">Household</option>
-            </select>
-      <ProductList category={category} />
+            </select> */}
+      {/* <ProductList category={category} /> */}
     </div>
   );
 }
