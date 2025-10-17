9ae4c178cfb00b1d117b236b04bbe73390556468
Dmitriy <mr.dima337@gmail.com>
2025-10-17 15:23:14 +0300

    feat: implement updating user

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -85,6 +85,17 @@ function App() {
         setUsers(originalUsers);
       });
   };
+  const updateUser = (user:User) => {
+    const originalUsers = [...users];
+    const updatedUser = {...user, name: user.name + '!'};
+    setUsers(users.map(u => u.id === user.id ? updatedUser : u));
+
+    axios.patch("https://jsonplaceholder.typicode.com/users/" + user.id, updatedUser)
+    .catch(err => {
+      setError(err.message);
+      setUsers(originalUsers)
+    })
+  }
 
   return (
     <>
@@ -100,12 +111,15 @@ function App() {
             key={user.id}
           >
             {user.name}
-            <button
-              onClick={() => deleteUser(user)}
-              className="btn btn-outline-danger"
-            >
-              Delete
-            </button>{" "}
+            <div>
+              <button className="btn btn-outline-secondary mx-1" onClick={()=> updateUser(user)}>Update</button>
+              <button 
+                onClick={() => deleteUser(user)}
+                className="btn btn-outline-danger"
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
         {/* <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1 }])} />
